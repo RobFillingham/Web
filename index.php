8d44baea313e609b63f56d05ecdf5296197a9753
FridaEscamilla02 <fridaescamilla0207@gmail.com>
2023-10-28 16:56:16 -0600

    Actualizacion

--- index.php
+++ index.php
@@ -1,1 +1,130 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="css/a1.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link
+    rel="stylesheet"
+     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+    </head>
+    <body>
 
+    <!-- Seccion Rombo 1 -->
+    <div class="section-container1">
+        <div class="table-container">
+
+            <div class="table-cell diamond">
+                <div data-aos="fade-right">
+                    <img src="img/img2.jpg" alt="" id="img">
+                </div>
+            </div>
+
+            <div class="table-cell">
+                <div class="text-container">
+                    <h4>Tecnologías</h4>
+                    <p>Descubre cómo nuestro equipo de desarrolladores</p>
+                    <p> trabaja con tecnologías de vanguardia.</p>
+                    <button>Ver Mas</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Seccion Rombo 2 -->
+    <div class="section-container2">
+        <div class="table-container">
+
+            <div class="table-cell">
+                <div class="text-container">
+                    <h4>Creatividad</h4>
+                    <p>Nuestro proceso de desarrollo web combina la creatividad y la experiencia</p>
+                    <p> técnica para crear sitios web efectivos y atractivos</p>
+                    <button>Ver Mas</button>
+                </div>
+            </div>
+
+            <div class="table-cell diamond">
+                <div data-aos="fade-up-left">
+                    <img src="img/img2.jpg" alt="" id="img2">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Seccion Rombo 3 -->
+    <div class="section-container3">
+        <div class="table-container">
+
+            <div class="table-cell diamond">
+                <div data-aos="fade-down-right">
+                    <img src="img/img3.jpg" alt="" id="img">
+                </div>
+            </div>
+
+            <div class="table-cell">
+                <div class="text-container">
+                    <h4>Actualizacion</h4>
+                    <p>Nuestra última aplicación en acción: una interfaz de usuario intuitiva</p>
+                    <p>y una experiencia fluida para los usuarios.</p>
+                    <button>Ver Mas</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="section-container4">
+    <table>
+        <tr>
+            <td>
+                <p id="text" class="animate__animated animate__backInLeft">Somos tu mejor opcion</p>
+            </td>
+
+            <td>
+                <div class="section-container-cards">
+                    <table>
+
+                        <tr>
+
+                            <td rowspan="5">
+                                <div class="card1">
+                                    <img src="img/icon1.jpg" alt="">
+                                    <h2>Trabajo en Equipo</h2>
+                                    <p>Trabajamos en armonía para componer soluciones de software excepcionales.</p>
+                                </div>
+                            </td>
+
+                            <td >
+                                <div class="card1">
+                                    <img src="img/icon2.jpg" alt="">
+                                    <h2>Desarrollo agil</h2>
+                                    <p> Saltamos los obstáculos, nos adaptamos al ritmo de los cambios y entregamos actuaciones sobresalientes en cada iteración.</p>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <div class="card1">
+                                    <img src="img/icon3.jpg" alt="">
+                                    <h2>Sistemas Empresariales</h2>
+                                    <p>Las empresas confían en nosotros para tejer conexiones invisibles y aumentar la eficiencia.</p>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </table>
+                </div>
+            </td>
+        </tr>
+   </table>
+   </div>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>AOS.init();</script>
+
+    </body>
+</html>
